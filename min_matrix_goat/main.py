8d18ce70d126870c836162c5e8cfdfa392c54326
Hanck Edd <hanckeddison@gmail.com>
2023-11-27 08:00:37 -0800

    base

---
+++ min_matrix_goat/main.py
@@ -0,0 +1,58 @@
+from itertools import permutations
+
+def min_multiplications(matrices_dimensiones, memo):
+    n = len(matrices_dimensiones)
+
+    if n in memo:
+        return memo[n]
+
+    if n == 1:
+        return 0
+
+    min_cost = float('inf')
+
+    for k in range(1, n):
+        q = min_multiplications(matrices_dimensiones[:k], memo) + min_multiplications(matrices_dimensiones[k:], memo) + matrices_dimensiones[0][0] * matrices_dimensiones[k-1][1] * matrices_dimensiones[-1][1]
+        min_cost = min(min_cost, q)
+
+    memo[n] = min_cost
+    return min_cost
+
+
+def is_valid_permutation(perm):
+    return all(perm[i][1] == perm[(i + 1) % len(perm)][0] for i in range(len(perm)))
+
+
+def min_mat_mult_norder(matrices_dimensiones):
+    perms = permutations(matrices_dimensiones)
+    valids = []
+    outputs = []
+
+    for perm in perms:
+        if is_valid_permutation(perm):
+            valids.append(list(perm))
+
+    if not valids:
+        print("Permutaciones no validas")
+        return
+
+    for valid_permutation in valids:
+        outputs.append(min_multiplications(valid_permutation, {}))
+
+    min_cost = min(outputs)
+    print("Minimum cost:", min_cost)
+
+
+# Matrices que no se pueden multiplicar
+matrices_dimensiones_imposible = [(2, 3), (3, 5), (5, 2), (2, 4), (4, 3)]
+print(matrices_dimensiones_imposible)
+min_mat_mult_norder(matrices_dimensiones_imposible)
+
+# Matrices que se pueden multiplicar
+matrices_dimensiones_posible = [(2, 3), (3, 5), (5, 2), (2, 4), (4, 6), (6, 2)]
+print(matrices_dimensiones_posible)
+min_mat_mult_norder(matrices_dimensiones_posible)
+
+matrices_dimensiones_posible = [(2, 3), (3, 5), (5, 2)]
+print(matrices_dimensiones_posible)
+min_mat_mult_norder(matrices_dimensiones_posible)
---
+++ min_matrix_op_hard/main.py
@@ -0,0 +1,46 @@
+from itertools import permutations
+
+def min_multiplications(matrices_dimensiones):
+    n = len(matrices_dimensiones)
+
+    # m[i][j] es el número mínimo de multiplicaciones escalares necesarias para
+    # calcular el producto de las matrices i a j.
+    # m[i][i] es 0 porque solo hay una matriz.
+    m = [[0 for _ in range(n)] for _ in range(n)]
+
+    # L es la longitud de la cadena de matrices
+    for L in range(2, n+1):
+        for i in range(n-L+1):
+            j = i+L-1
+            m[i][j] = float('inf')
+            for k in range(i, j):
+                q = m[i][k] + m[k+1][j] + matrices_dimensiones[i][0]*matrices_dimensiones[k][1]*matrices_dimensiones[j][1]
+                if q < m[i][j]:
+                    m[i][j] = q
+
+    return m[0][n-1]
+
+
+
+def min_mat_mult_norder(matrices_dimensiones):
+    perms = permutations(matrices_dimensiones)
+    valids = []
+    outputs = []
+
+    for perm in perms:
+        # Verificar si la permutación es válida
+        valid_perm = all(perm[i][1] == perm[(i + 1) % len(perm)][0] for i in range(len(perm)))
+
+        if valid_perm:
+            valids.append(list(perm))
+
+    for valid_permutation in valids:
+        print(valid_permutation)
+        outputs.append(min_multiplications(valid_permutation))
+
+    min_cost = min(outputs)
+    print("Minimum cost:", min_cost)
+
+matrices_dimensiones = [(2, 3), (3, 5), (5, 2)]
+#matrices_dimensiones = [(5, 2), (2, 3), (3, 5)]
+min_mat_mult_norder(matrices_dimensiones)
